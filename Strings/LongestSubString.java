5fb117e4f52fd99b46ab2a63c9a7bde6f46bcc68
AdiGo22 <coderadigo@gmail.com>
2024-11-29 18:20:08 +0530

    first commit

---
+++ Strings/LongestSubString.java
@@ -0,0 +1,27 @@
+package Strings;
+
+import java.util.*;
+
+public class LongestSubString {
+    public static int longest(String s) {
+        int maxCount = 0; // To store maximum window size
+        int l = 0; // Left pointer of the sliding window
+        HashSet<Character> set = new HashSet<>();
+        
+        for (int r = 0; r < s.length(); r++) { // Right pointer of the sliding window
+            while (set.contains(s.charAt(r))) {
+                set.remove(s.charAt(l)); // Remove left-most character to shrink window
+                l++;
+            }
+            set.add(s.charAt(r)); // Add the current character
+            maxCount = Math.max(maxCount, r - l + 1); // Update maximum length
+        }
+        return maxCount;
+    }
+    
+    public static void main(String[] args) {
+        String s = "pwwkew";
+        System.out.println(longest(s)); // Output: 3 ("wke" or "kew")
+    }
+}
+
---
+++ Strings/LongestSubString2.java
@@ -0,0 +1,27 @@
+package Strings;
+
+import java.util.HashMap;
+
+public class LongestSubString2 {
+    //optimised version of Longest SubStrig solution
+
+    public static int Longest(String s) {
+        int l = 0,maxCount =0;
+        HashMap<Character,Integer>map = new HashMap<>();
+        for(int r=0;r<s.length();r++){
+            char current = s.charAt(r);
+            if(map.containsKey(current)){
+                //if duplicate found
+                //move the l pointer
+                l = Math.max(map.get(current)+1 ,l);
+            }
+            map.put(current,r);
+            maxCount=Math.max(r-l+1,maxCount);
+        }
+        return maxCount;
+    }
+    public static void main(String[] args) {
+        String s = "pwwkew";
+        System.out.println(Longest(s));
+    }
+}
